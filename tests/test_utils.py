4d8fdcb5545aca763f1ec4ab497e5fb15267ea0f
Danis Akhmadiev <iamdanisakhmadiev>
2024-03-03 14:10:18 +0400

    added fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,18 @@
 from utils import dicts
+import pytest
 
 def test_get_val():
     assert dicts.get_val({0: 'Python', 1: 'Java', 2: 'R'}, 0 ) == 'Python'
     assert dicts.get_val({0: 'Python', 1: 'Java', 2: 'R'}, 8) == 'git'
     assert dicts.get_val({0: 'Python', 1: 'Java', 2: 'R'}, -7, 'NO') == 'NO'
+
+@pytest.fixture
+def fixt():
+    return {0: 'Python', 1: 'Java', 2: 'R', 3: 'JS', 4: 'C#', 5: 'C++'}
+
+def test_get_val_fixt1():
+    assert dicts.get_val(fixt, 1) == 'Java'
+
+def test_get_val_fixt2():
+    assert dicts.get_val(fixt, 3) != 'git'
+
